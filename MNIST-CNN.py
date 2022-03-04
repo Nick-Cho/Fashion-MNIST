1bdd86b8fbbb582514653355580132bfc94da7a8
Nick Cho <65980644+Nick-Cho@users.noreply.github.com>
2022-03-04 15:06:38 -0500

    Building initial model with ~90% accuracy

---
+++ MNIST-CNN.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.layers import Input, Conv2D, Dense, Flatten, Dropout
+from tensorflow.keras.models import Model
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+
+# Data Processing
+
+# Scaling x variables
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# for i in range(5):
+#   print("X_train.shape: ", x_train[i], "\n")
+
+# Adding a dimension to satisfy input shape of height x width x color
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
+
+# print("Without Set: ", len(y_train))
+# print ("With Set: ", len(set(y_train)))
+K = len(set(y_train))
+
+# Model Building
+i = Input(shape=(x_train[0].shape))
+x = Conv2D(32, (3, 3), strides=2, activation='relu')(i)
+x = Conv2D(64, (3, 3), strides=2, activation='relu')(x)
+x = Conv2D(128, (3, 3), strides=2, activation='relu')(x)
+x = Flatten()(x)
+x = Dropout(0.3)(x)
+x = Dense(512, activation='relu')(x)
+x = Dropout(0.3)(x)
+x = Dense(K, activation='softmax')(x)
+
+model = Model(i, x)
+
+model.compile(optimizer="adam",
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+r = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=15)
